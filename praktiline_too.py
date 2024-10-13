11294f2418ecb8057149207b8df4060ff6ea390c
Diana-vastsenko <diana.vastsenko@gmail.com>
2024-10-13 12:19:10 +0300

    Update praktiline_too.py

--- praktiline_too.py
+++ praktiline_too.py
@@ -4,8 +4,11 @@
 # 1) küsib kasutajalt ainult täisarve a ja b
 # 2) tagastab kasutajale a ja b summa, korrutise ja ruutude summa
 # all on sulle vastuse näide koos andmetüübiga f-sõne, proovi jutumärkide sisse panna loogsulud {}
+print("Sisesta a:")
+a=int(input())
+print("Sisesta b:")
+b=int(input(int))
 
-
-print(f"SUMMA:")
-print(f"KORRUTIS:")
-print(f"RUUTUDE SUMMA:")
+print(f"SUMMA: {a + b}")
+print(f"KORRUTIS: {a * b}")
+print(f"RUUTUDE SUMMA: {a**2 + b**2}")
